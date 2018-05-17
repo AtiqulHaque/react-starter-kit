5075784b95644887e0e1bcc0a2faf4679067e7da
atik <md_atiqulhaque@yahoo.com>
2018-05-17 15:48:42 +0600

    add all the dependency for project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../public/custom.css';
+const Root = () => {
+  return (
+    <h1>Hello World hiagaing</h1>
+  )
+}
+ReactDOM.render(<Root />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+ module: {
+  rules: [
+    {
+      test: /\.js$/, 
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },{
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+    },
+	{
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            }
+          }
+      ]
+    }
+  ]
+}
+}
